50fd42ed4220095c74c57467964cc899a7d62a39
Paul de Mast <pjfmast@gmail.com>
2023-04-25 12:52:20 +0200

    apply mvvm pattern

--- TodoREST/Views/TodoListPage.xaml.cs
+++ TodoREST/Views/TodoListPage.xaml.cs
@@ -4,19 +4,11 @@ namespace TodoREST.Views
 {
     public partial class TodoListPage : ContentPage
     {
-        //private readonly TodoListViewModel _todoListViewModel;
         public TodoListPage(TodoListViewModel todoListViewModel)
         {
             InitializeComponent();
-            //_todoListViewModel = todoListViewModel;
             BindingContext = todoListViewModel;
         }
-
-        //protected async override void OnAppearing()
-        //{
-        //    base.OnAppearing();
-        //    await _todoListViewModel.RefreshTodoItems();
-        //}
 
     }
 }
